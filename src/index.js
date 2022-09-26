77d0fdd37c3df983c6ae7c23599b9f579b72ea06
KhalidAbbasJan <arazaq917@gmail.com>
2022-09-26 12:41:01 +0500

    store created

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './App';
 
 const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
