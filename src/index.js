34736084f140ad2d7fd6305249d777687453fab4
KhalidAbbasJan <arazaq917@gmail.com>
2022-09-26 11:24:03 +0500

    store created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import {createStore} from 'redux';
+import allReducer from "./fabric-editor/reducers";
+
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
