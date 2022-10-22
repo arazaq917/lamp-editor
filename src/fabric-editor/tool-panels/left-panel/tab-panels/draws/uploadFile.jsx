7b0f93d2cc7b650213ae4c1a85cf4926ae6911f3
Osama <osamakhn97@gmail.com>
2022-10-22 13:44:10 +0500

    images uploaded on canvas after click

--- src/fabric-editor/tool-panels/left-panel/tab-panels/draws/uploadFile.jsx
+++ src/fabric-editor/tool-panels/left-panel/tab-panels/draws/uploadFile.jsx
@@ -5,7 +5,7 @@ import img1 from '../../../../../assets/images/bg.png'
 import img2 from '../../../../../assets/images/PNG Various/baby.png'
 
 const UploadFilePanel = () => {
-    const [imageData, setImageData] = useState([img2,img1])
+    const [imageData, setImageData] = useState([])
     const inputRef = useRef()
     const canvas = useSelector(state => state.canvas)
 
@@ -17,7 +17,7 @@ const UploadFilePanel = () => {
         const reader = new FileReader();
         reader.readAsDataURL(uploadedImageData);
         reader.onload = (e)=> {
-            var imgObj = new Image();
+            let imgObj = new Image();
             imgObj.src = e.target.result;
             imgObj.onload = function () {
                 var image = new fabric.Image(imgObj);
@@ -34,6 +34,21 @@ const UploadFilePanel = () => {
             ]);
         };
     }
+    const addImage = (item)=>{
+        console.log(item)
+        let imgObj = new Image();
+        imgObj.src = item;
+        imgObj.onload = function () {
+            var image = new fabric.Image(imgObj);
+            image.set({
+                scaleX:0.3,
+                scaleY:0.3,
+            });
+            canvas.centerObject(image);
+            canvas.add(image);
+            canvas.renderAll();
+        }
+    }
 
     return(
         <div className="file_panel_wrapper">
@@ -45,7 +60,7 @@ const UploadFilePanel = () => {
                 {imageData.length &&
                     imageData.map((item,index)=> {
                         return (
-                            <div key={index} className="images-div">
+                            <div key={index} className="images-div" onClick={()=>addImage(item)}>
                                 <div className="container">
                                     <div className="row">
                                         <div className="col">
