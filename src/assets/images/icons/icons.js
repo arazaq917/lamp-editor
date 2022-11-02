caecf157d6018baddefa2d4a7645631fdb86aa04
Abdul Razaq <arazaq917@gmail.com>
2022-11-02 23:57:25 +0500

    undo redo, gid, froward backward

---
+++ src/assets/images/icons/icons.js
@@ -0,0 +1,53 @@
+export const FlipFront = () => {
+    return (
+        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M464,16H144a32.036,32.036,0,0,0-32,32V368a32.036,32.036,0,0,0,32,32H464a32.036,32.036,0,0,0,32-32V48A32.036,32.036,0,0,0,464,16Zm0,352H144V48H464l.021,320Z"/>
+            <rect width="30.77" height="32" x="77.846" y="464"/>
+            <rect width="30.77" height="32" x="384" y="464"/>
+            <rect width="30.77" height="32" x="200.308" y="464"/>
+            <rect width="30.769" height="32" x="139.077" y="464"/>
+            <rect width="30.769" height="32" x="261.539" y="464"/>
+            <rect width="30.77" height="32" x="322.769" y="464"/>
+            <rect width="32" height="32" x="16" y="464"/>
+            <rect width="32" height="30.154" x="16" y="224.308"/>
+            <rect width="32" height="30.154" x="16" y="284.461"/>
+            <rect width="32" height="30.154" x="16" y="404.769"/>
+            <rect width="32" height="30.154" x="16" y="344.615"/>
+            <rect width="32" height="30.154" x="16" y="164.154"/>
+            <rect width="32" height="30.154" x="16" y="104"/>
+        </svg>
+
+    )
+}
+
+export const FlipBottom = () => {
+    return (
+        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M64,496H400V464H64a16.019,16.019,0,0,1-16-16V120H16V448A48.055,48.055,0,0,0,64,496Z"/>
+            <rect width="32" height="32" x="464" y="368"/>
+            <rect width="29.091" height="32" x="173.091" y="368"/>
+            <rect width="29.091" height="32" x="405.818" y="368"/>
+            <rect width="29.091" height="32" x="231.272" y="368"/>
+            <rect width="29.091" height="32" x="289.455" y="368"/>
+            <rect width="29.091" height="32" x="347.637" y="368"/>
+            <rect width="32" height="32" x="112" y="368"/>
+            <rect width="32" height="29.091" x="112" y="135.272"/>
+            <rect width="32" height="29.091" x="112" y="77.091"/>
+            <rect width="32" height="29.091" x="112" y="309.818"/>
+            <rect width="32" height="29.091" x="112" y="251.637"/>
+            <rect width="32" height="29.091" x="112" y="193.455"/>
+            <rect width="32" height="32" x="112" y="16"/>
+            <rect width="29.091" height="32" x="289.454" y="16"/>
+            <rect width="29.091" height="32" x="231.272" y="16"/>
+            <rect width="29.091" height="32" x="405.818" y="16"/>
+            <rect width="29.091" height="32" x="173.091" y="16"/>
+            <rect width="29.091" height="32" x="347.637" y="16"/>
+            <rect width="32" height="32" x="464" y="16"/>
+            <rect width="32" height="29.091" x="464" y="309.818"/>
+            <rect width="32" height="29.091" x="464" y="77.091"/>
+            <rect width="32" height="29.091" x="464" y="193.454"/>
+            <rect width="32" height="29.091" x="464" y="135.272"/>
+            <rect width="32" height="29.091" x="464" y="251.637"/>
+        </svg>
+    )
+}
--- src/fabric-editor/tool-baar/ToolBaar.jsx
+++ src/fabric-editor/tool-baar/ToolBaar.jsx
@@ -1,10 +1,156 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css'
+import { Tooltip } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import {FlipBottom, FlipFront} from "../../assets/images/icons/icons";
+import redoImg from "../../assets/images/forward.png";
+import undoImg from "../../assets/images/undo.png";
+import grid from "../../assets/images/grid.png";
+import backward from "../../assets/images/backward.png";
+import Forward from "../../assets/images/sendForward.png";
+import { fabric } from "fabric";
+import "fabric-history";
+import {useSelector} from "react-redux";
 const ToolBaar =()=>{
+    const [gridEnabled, setGridEnabled] = useState(false);
+    const [overlayGrid, setOverlayGrid] = useState();
+    const canvas = useSelector(state => state.canvas)
+    const sendForward = () => {
+        let obj = canvas.getActiveObject();
+        canvas.bringForward(obj);
+        canvas.renderAll()
+    }
+
+    const sendBackward = () => {
+        let obj = canvas.getActiveObject();
+        canvas.sendBackwards(obj);
+        canvas.renderAll()
+    }
+    const redo = () => {
+        canvas.redo();
+    }
+    const undo = () => {
+        canvas.undo();
+    }
+
+    const makeGrid = () => {
+        let columns = 5;
+        let rows = 5;
+        const div = 5;
+        const height = canvas.height - 2;
+        const width = canvas.width - 2;
+        const verticalDistance = height / rows;
+        const horizontalDistance = width / columns;
+        const divVertical = verticalDistance / div;
+        const divHorizontal = horizontalDistance / div;
+        let lines = [];
+        let newLines = []
+        let rect = new fabric.Rect({
+            left: 0, top: 0, height: height, width: width, stroke: "dark gray", fill: null,
+        })
+        for (let i = 0; i < columns; i++) {
+            for (let k = 1; k < div; k++) {
+                let line = new fabric.Line([i * horizontalDistance + k * divHorizontal, 0, i * horizontalDistance + k * divHorizontal, height], {
+                    left: i * horizontalDistance + k * divHorizontal,
+                    top: 0,
+                    stroke: '#808080',
+                    strokeDashArray: [3, 3],
+                });
+                lines.push(line);
+            }
+            if (i != 0) {
+                let line = new fabric.Line([i * horizontalDistance, 0, i * horizontalDistance, height], {
+                    left: i * horizontalDistance,
+                    top: 0,
+                    stroke: 'dark gray'
+                });
+                newLines.push(line);
+            }
+
+        }
+        for (let j = 0; j < rows; j++) {
+            for (let k = 1; k < div; k++) {
+                let line = new fabric.Line([0, j * verticalDistance + k * divVertical, width, j * verticalDistance + k * divVertical], {
+                    left: 0,
+                    top: j * verticalDistance + k * divVertical,
+                    strokeDashArray: [3, 3],
+                    stroke: '#808080',
+                });
+                lines.push(line);
+            }
+            if (j != 0) {
+                let line = new fabric.Line([0, j * verticalDistance, width, j * verticalDistance], {
+                    left: 0,
+                    top: j * verticalDistance,
+                    stroke: 'dark gray'
+                });
+                newLines.push(line);
+            }
+
+        }
+        lines = [...lines, ...newLines]
+        let group = new fabric.Group([rect, ...lines]);
+
+        setOverlayGrid(group)
+        return group;
+    }
+
+    const showGrid=()=>{
+
+        if (!gridEnabled){
+            if(!overlayGrid) {
+                setOverlayGrid(makeGrid());
+            }
+
+            //to stop repetitive behaviour
+            if(gridEnabled)
+                return;
+
+            canvas.setOverlayImage(overlayGrid, () => {
+                canvas.renderAll();
+            }, {
+                overlayImageLeft: 0,
+                overlayImageTop: 0,
+            });
+            setGridEnabled(!gridEnabled)
+        }
+        else{
+            canvas.overlayImage = null;
+            canvas.renderAll();
+            setGridEnabled(!gridEnabled)
+        }
+    }
 
     return (
         <div className={`toolbaar-container`}>
+            <div className="canvas-tools">
+                <Button variant="light" onClick={undo}>
+                    <span>
+                        <img src={undoImg} height={25} width={25}/>
+                    </span>
+                </Button>
+                <Button variant="light" onClick={redo}>
+                    <span>
+                        <img src={redoImg} height={25} width={25}/>
+                    </span>
+                </Button>
+                <Button variant="light" onClick={sendForward}>
+                    <span>
+                        <img src={Forward} height={25} width={25}/>
+                    </span>
+                </Button>
+                <Button variant="light" onClick={sendBackward}>
+                    <span>
+                        <img src={backward} height={25} width={25}/>
+                    </span>
+                </Button>
+                <Button variant="light" onClick={showGrid}>
+                    <span>
+                        <img src={grid} height={25} width={25}/>
+                    </span>
+                </Button>
 
+            </div>
         </div>
     );
 }
