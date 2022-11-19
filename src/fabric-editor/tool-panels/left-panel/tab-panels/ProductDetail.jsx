6d6fb15a5e7d8e9b41838cd0e798cf69907faf6c
Osama <osamakhn97@gmail.com>
2022-11-19 21:59:36 +0500

    trim colors

--- src/fabric-editor/tool-panels/left-panel/tab-panels/ProductDetail.jsx
+++ src/fabric-editor/tool-panels/left-panel/tab-panels/ProductDetail.jsx
@@ -1,23 +1,29 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './index.css'
 import {useDispatch, useSelector} from "react-redux";
 import {captureShots} from "../../../../utils/bounds";
 import {setPreviewState, setTrimColor} from "../../../actions";
 import img1 from "../../../../assets/images/Tripod.jpg";
 import img2 from "../../../../assets/images/White Base.jpg";
+import moreBtn from "../../../../assets/images/ButtonsImages/more.png";
 
-let COLORS = ["#fff", "#003E60", "#F7CF00", "#C40E12", "#23803C"]
+let COLORS = ["#1a1a1a", "#c1c0be", "#cabeae", "#f6f6f6", "#fad3e6","#fad3e6","#f98831","#a1cee2","#9fcc93","#f3e06f","#dd4f53","#255487","#8d1725"]
 
 const ProductDetail =()=>{
     const previewStyle = useSelector(state => state.previewSwitch)
 
     const dispatch = useDispatch()
+    const colorPicker = useRef();
 
     const lampTrimColor = (color) =>{
         dispatch(setTrimColor(color))
     }
+    const handleColorChange = ()=>{
+        colorPicker.current.click();
+    }
 
     const newColor = (e) =>{
+        console.log('color changed')
         dispatch(setTrimColor(e.target.value))
     }
     const toggleDesign = (type)=>{
@@ -28,7 +34,10 @@ const ProductDetail =()=>{
             <div>
                 <span className="formatted_title">Trim Colors</span>
                 <div className="colors_container">
-                    <input id="color" type="color" onChange={newColor} className={'colors_color'}/>
+                    <div className="colors_color black__border_light" onClick={()=>handleColorChange()}>
+                        <img src={moreBtn} style={{ width: 40, height: 40}}/>
+                    </div>
+                    <input style={{display:"none"}} ref={colorPicker} id="color" type="color" onChange={newColor} className={'colors_color'}/>
                     {COLORS.map(color => <div key={color}
                           data-color={color}
                           onClick={() => lampTrimColor(color)}
